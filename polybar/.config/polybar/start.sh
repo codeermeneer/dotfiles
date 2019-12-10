cf23dad663a8404938425c4429463de8c3b89402
Toto0109 <codeermeneer@gmail.com>
2019-12-10 17:33:49 +0100

    inital commit

---
+++ polybar/.config/polybar/start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+killall -q polybar
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+echo "---" | tee -a /tmp/polybar1.log
+polybar lit >>/tmp/polybar1.log 2>&1 &
+
+echo "polybar started"
